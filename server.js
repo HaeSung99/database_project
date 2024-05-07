38767133d57d52c20867e87824dd5be2674d1181
성인석 <inseok1999@gmail.com>
2024-05-07 16:32:35 +0900

    Revert "회원가입기능 만들었음"
    This reverts commit e52c2220eeec48235dd4681bfe9cd9e7999977bb.

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql2');
-app.use(express.json());
-app.use(express.urlencoded({extended:true}));
 
 
 app.set('view engine', 'ejs');
@@ -12,7 +10,7 @@ const db = mysql.createConnection({
 host : 'localhost',  
 user : 'root',
 password : 'haesung8494@',
-database : 'project'
+database : 'company'
 });
 
 db.connect(function(err) {
@@ -33,31 +31,6 @@ app.get('/', (요청, 응답) =>{
         console.log(결과);
         응답.render('main.ejs',{ data : 결과 })
   });
-});
-
-app.get('/sign', (요청,응답) =>{
-  응답.render('sign.ejs')
-})
-
-// app.post('/sign',(요청,응답) =>{
-//   console.log(요청.body.username)
-// })
-
-//회원가입 정보받는곳
-app.post('/sign', (요청,응답)=>{
-  const { username,  password, nickname } = 요청.body;
-
-  // 데이터베이스에 새로운 회원 정보 추가
-  const sql = 'INSERT INTO member (username, password, nickname) VALUES (?, ?, ?)';
-  db.query(sql, [username, password, nickname], (에러, 결과) => {
-      if (에러) {
-          console.error('회원정보추가실패: ' + 에러.stack);
-          return;
-      }
-      console.log('회원번호: ', 결과.insertId);
-      응답.send('회원가입에 성공했습니다.');
-  });
-
 });
     
 
